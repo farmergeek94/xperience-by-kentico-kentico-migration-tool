881c8ad61e0f5b84e011dd1112715725bdde4c2d
Joshua Hess <jhess@hbs.net>
2024-10-17 14:03:17 -0500

    Update

--- KVA/Migration.Tool.Source/Handlers/MigrateCustomTablesHandler.cs
+++ KVA/Migration.Tool.Source/Handlers/MigrateCustomTablesHandler.cs
@@ -173,7 +173,7 @@ public class MigrateCustomTablesHandler(
                         // get all the data for type
                         var data = Data(xbkDataClass.ClassTableName);
 
-                        string languageCode = "en";
+                        string languageCode = modelFacade.SelectAll<ICmsCulture>().FirstOrDefault(x => x.CultureIsUICulture == true)?.CultureCode ?? "en";
 
                         // create content items from data
                         foreach (var item in data)
